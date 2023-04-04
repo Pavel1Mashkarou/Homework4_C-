8c12df6d27b40a2108fe0d83b836538efdaf94aa
Pavel <pmaskarev@vk.com>
2023-04-04 10:47:30 +0300

    Added task027

---
+++ Task027/Program.cs
@@ -0,0 +1,21 @@
+﻿int Start()
+{
+    System.Console.WriteLine("Введите число:");
+    int a = int.Parse(Console.ReadLine());
+    return a; 
+}
+
+int a = Start();
+
+void Summ(int a)
+{
+    int result = 0;
+    while (a!=0)
+    {
+        result = result + a % 10;
+        a = a / 10;
+    }
+    System.Console.WriteLine(result);
+}
+
+Summ(a);
