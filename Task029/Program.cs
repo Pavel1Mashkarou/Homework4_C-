b9bb3e07a3091c43757ec149daa018c05c0d44af
Pavel <pmaskarev@vk.com>
2023-04-04 11:07:42 +0300

    Added task029

---
+++ Task029/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] array = new int[8];
+
+void Magic(int[] array)
+{
+    for (int a = 0; a < array.Length; a++)
+    {
+        Console.WriteLine("Введите число:");
+        array[a] = int.Parse(Console.ReadLine());
+    }
+    Console.Write("[");
+    for (int a = 0; a < array.Length; a++)
+    {
+        Console.Write(array[a]);
+        if (a < array.Length - 1)
+        {
+            Console.Write(";");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+Magic(array);
