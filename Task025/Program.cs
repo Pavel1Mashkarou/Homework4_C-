97122910f008b0eff5bf6d6598c786d15f1e40b8
Pavel <pmaskarev@vk.com>
2023-04-04 10:06:16 +0300

    Added task025

---
+++ Task025/Program.cs
@@ -0,0 +1,21 @@
+﻿int Start(string arg)
+{
+    System.Console.WriteLine($"Введите {arg}");
+    int a = int.Parse(Console.ReadLine());
+    return a; 
+}
+
+int a = Start("число, которое будем вводить в степень");
+int n = Start("степень числа:");
+
+void Magic(int a, int n)
+{
+    int result = a;
+    for (int i = 2; i<=n; i++)
+    {
+        result = result * a;
+    }
+    System.Console.WriteLine($"Число {a} в степени {n} будет равно: {result}");
+}
+
+Magic(a,n);
